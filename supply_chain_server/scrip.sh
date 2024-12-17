ee5e28b6d5b0c4b0b4cd8be47d93a28a51bcb0fe
sivasubramani <sivasubramani.bsiva@gmail.com>
2024-12-17 15:18:16 +0530

    Add files via upload

---
+++ supply_chain_server/scrip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "order_001.csv"
+
+echo "the things was ordered and  delivered  by  our  "
+echo "company delivery man on yesterday  "
+echo "with out any damage and delivered safely"
+
+
+echo "shipment_100.csv"
+
+echo "the things was shipped four"
+echo "days after the customer was"
+echo "order the product  in  our company"
+ 
